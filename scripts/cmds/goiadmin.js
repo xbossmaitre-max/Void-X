13322923b5ee424fe7ec05c795224fe48a26b129
CHITRON BHATTACHARJEE <162599803+brandchitron@users.noreply.github.com>
2025-06-15 08:09:45 +0600

    Add goiadmin.js via Messenger Goat Bot

---
+++ scripts/cmds/goiadmin.js
@@ -0,0 +1,24 @@
+module.exports = {
+	config: {
+		name: "goiadmin",
+		author: "NIsAN",
+		role: 0,
+		shortDescription: " ",
+		longDescription: "",
+		category: "BOT",
+		guide: "{pn}"
+	},
+
+onChat: function({ api, event }) {
+	if (event.senderID !== "100081330372098") {
+		var aid = ["100081330372098"];
+		for (const id of aid) {
+		if ( Object.keys(event.mentions) == id) {
+			var msg = ["হু, আমার দেখা পাওয়ার জন্য অপেক্ষা করো। 🦆 "];
+			return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
+		}
+		}}
+},
+onStart: async function({}) {
+	}
+};
