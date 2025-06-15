056eb87e8844c4bdc7d3e6c4762d619e95ee1e3e
CHITRON BHATTACHARJEE <162599803+brandchitron@users.noreply.github.com>
2025-06-15 08:38:09 +0600

    Update translate.js via Messenger Goat Bot

--- scripts/cmds/translate.js
+++ scripts/cmds/translate.js
@@ -15,20 +15,20 @@ module.exports = {
 		},
 		category: "utility",
 		guide: {
-			vi: "   {pn} <văn bản>: Dịch văn bản sang ngôn ngữ của box chat bạn hoặc ngôn ngữ mặc định của bot"
-				+ "\n   {pn} <văn bản> -> <ISO 639-1>: Dịch văn bản sang ngôn ngữ mong muốn"
-				+ "\n   hoặc có thể phản hồi 1 tin nhắn để dịch nội dung của tin nhắn đó"
-				+ "\n   Ví dụ:"
-				+ "\n    {pn} hello -> vi"
-				+ "\n   {pn} -r [on | off]: Bật hoặc tắt chế độ tự động dịch tin nhắn khi có người thả cảm xúc vào tin nhắn"
-				+ "\n   {pn} -r set <emoji>: Đặt emoji để dịch tin nhắn trong nhóm chat của bạn",
-			en: "   {pn} <text>: Translate text to the language of your chat box or the default language of the bot"
-				+ "\n   {pn} <text> -> <ISO 639-1>: Translate text to the desired language"
-				+ "\n   or you can reply a message to translate the content of that message"
-				+ "\n   Example:"
-				+ "\n    {pn} hello -> vi"
-				+ "\n   {pn} -r [on | off]: Turn on or off the automatic translation mode when someone reacts to the message"
-				+ "\n   {pn} -r set <emoji>: Set the emoji to translate the message in your chat group"
+			vi: " {pn} <văn bản>: Dịch văn bản sang ngôn ngữ của box chat bạn hoặc ngôn ngữ mặc định của bot"
+				+ "\n {pn} <văn bản> -> <ISO 639-1>: Dịch văn bản sang ngôn ngữ mong muốn"
+				+ "\n hoặc có thể phản hồi 1 tin nhắn để dịch nội dung của tin nhắn đó"
+				+ "\n Ví dụ:"
+				+ "\n {pn} hello -> vi"
+				+ "\n {pn} -r [on | off]: Bật hoặc tắt chế độ tự động dịch tin nhắn khi có người thả cảm xúc vào tin nhắn"
+				+ "\n {pn} -r set <emoji>: Đặt emoji để dịch tin nhắn trong nhóm chat của bạn",
+			en: " {pn} <text>: Translate text to the language of your chat box or the default language of the bot"
+				+ "\n {pn} <text> -> <ISO 639-1>: Translate text to the desired language"
+				+ "\n or you can reply a message to translate the content of that message"
+				+ "\n Example:"
+				+ "\n {pn} hello -> vi"
+				+ "\n {pn} -r [on | off]: Turn on or off the automatic translation mode when someone reacts to the message"
+				+ "\n {pn} -r set <emoji>: Set the emoji to translate the message in your chat group"
 		}
 	},
 
