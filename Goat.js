7bad49ebc3fad53abf54545b4d3b4985cbbcf11d
CHITRON BHATTACHARJEE <162599803+brandchitron@users.noreply.github.com>
2025-06-18 01:29:42 +0600

    Update Goat.js

--- Goat.js
+++ Goat.js
@@ -16,6 +16,7 @@
  * ! Nếu thay đổi nó, bạn sẽ bị cấm vĩnh viễn
  * Cảm ơn bạn đã sử dụng
  */
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 process.on('unhandledRejection', error => console.log(error));
 process.on('uncaughtException', error => console.log(error));
