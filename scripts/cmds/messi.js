222d30f65225503a4dd9f4f20981964e0ffb2fea
CHITRON BHATTACHARJEE <162599803+brandchitron@users.noreply.github.com>
2025-06-28 00:02:09 +0600

    Update messi.js via Messenger Goat Bot

--- scripts/cmds/messi.js
+++ scripts/cmds/messi.js
@@ -1,74 +1,70 @@
-
 module.exports = {
-  config: {
-    name: "messi",
-    aliases: ["lm10"],
-    version: "1.0",
-    author: "Otineyyyy your dadddy👾😉",
-    countDown: 5,
-    role: 0,
-    shortDescription: "send you pic of messi",
-    longDescription: "",
-    category: "football",
-    guide: "{pn}"
-  },
+ config: {
+ name: "messi",
+ aliases: ["lm10"],
+ version: "1.0",
+ author: "Chitron Bhattacharjee",
+ countDown: 5,
+ role: 0,
+ shortDescription: {
+ en: "Send a random photo of Messi"
+ },
+ longDescription: {
+ en: "Sends both image and URL of a random Lionel Messi photo"
+ },
+ category: "football",
+ guide: {
+ en: "{pn}"
+ }
+ },
 
-  onStart: async function ({ message }) {
-   var link = [ "https://i.imgur.com/ahKcoO3.jpg",
+ onStart: async function ({ message }) {
+ try {
+ const fs = require("fs-extra");
+ const path = require("path");
+ const https = require("https");
+ 
+ const links = [
+ "https://i.imgur.com/ahKcoO3.jpg",
+ "https://i.imgur.com/Vsf4rM3.jpg",
+ "https://i.imgur.com/ximEjww.jpg",
+ "https://i.imgur.com/ukYhm0D.jpg",
+ "https://i.imgur.com/Poice6v.jpg",
+ "https://i.imgur.com/5yMvy5Z.jpg",
+ "https://i.imgur.com/ndyprcd.jpg",
+ "https://i.imgur.com/Pm2gC6I.jpg",
+ "https://i.imgur.com/wxxHuAG.jpg",
+ "https://i.imgur.com/GwOCq59.jpg"
+ ];
 
-"https://i.imgur.com/Vsf4rM3.jpg",
+ const randomLink = links[Math.floor(Math.random() * links.length)];
+ const imgName = `messi_${randomLink.split('/').pop()}`;
+ const imgPath = path.join(__dirname, "cache", imgName);
 
-"https://i.imgur.com/ximEjww.jpg",
+ if (!fs.existsSync(imgPath)) {
+ await new Promise((resolve, reject) => {
+ https.get(randomLink, (res) => {
+ const fileStream = fs.createWriteStream(imgPath);
+ res.pipe(fileStream);
+ fileStream.on("finish", () => {
+ fileStream.close();
+ resolve();
+ });
+ }).on("error", (err) => {
+ fs.unlinkSync(imgPath);
+ reject(err);
+ });
+ });
+ }
 
-"https://i.imgur.com/ukYhm0D.jpg",
+ message.reply({
+ body: `「 The GOAT has arrived 🐐 」\nImage URL: ${randomLink}`,
+ attachment: fs.createReadStream(imgPath)
+ });
 
-"https://i.imgur.com/Poice6v.jpg",
-
-"https://i.imgur.com/5yMvy5Z.jpg",
-
-"https://i.imgur.com/ndyprcd.jpg",
-
-"https://i.imgur.com/Pm2gC6I.jpg",
-
-"https://i.imgur.com/wxxHuAG.jpg",
-
-"https://i.imgur.com/GwOCq59.jpg",
-
- "https://i.imgur.com/oM0jc4i.jpg",
-
-"https://i.imgur.com/dJ0OUef.jpg",
-
-"https://i.imgur.com/iurRGPT.jpg",
-
-"https://i.imgur.com/jogjche.jpg",
-
-"https://i.imgur.com/TiyhKjG.jpg",
-
-"https://i.imgur.com/AwlBM23.jpg",
-
-"https://i.imgur.com/9OLSXZD.jpg",
-
-
-"https://i.imgur.com/itscmiy.jpg",
-
-"https://i.imgur.com/FsnCelU.jpg",
-
-"https://i.imgur.com/c7BCwDF.jpg",
-
-"https://i.imgur.com/3cnR6xh.jpg",
-
-"https://i.imgur.com/TZqepnU.jpg",
-
-"https://i.imgur.com/kYxEPrD.jpg",
-
-"https://i.imgur.com/9ZjD5nX.jpg",
-
-"https://i.imgur.com/YWyI4hP.jpg"
-]
-
-let img = link[Math.floor(Math.random()*link.length)]
-message.send({
-  body: '「 The Goat has arrived🐐 」',attachment: await global.utils.getStreamFromURL(img)
-})
-}
-     }
+ } catch (error) {
+ console.error("Error sending Messi image:", error);
+ message.send("Sorry, couldn't send the Messi image right now. Try again later!");
+ }
+ }
+};
