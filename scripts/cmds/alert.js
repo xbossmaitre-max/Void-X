2d41c23848c4e01be043fc0ef6db938c1128f674
CHITRON BHATTACHARJEE <162599803+brandchitron@users.noreply.github.com>
2025-06-28 13:33:20 +0600

    Update category of alert to "𝗙𝗨𝗡 & 𝗚𝗔𝗠𝗘"

--- scripts/cmds/alert.js
+++ scripts/cmds/alert.js
@@ -15,7 +15,7 @@ module.exports = {
     description: {
       en: "Generates an alert style meme image using your text"
     },
-    category: "fun",
+    category: "𝗙𝗨𝗡 & 𝗚𝗔𝗠𝗘",
     guide: {
       en: "{p}alert <text>\nExample: {p}alert Warning!"
     }
