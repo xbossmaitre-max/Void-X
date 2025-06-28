20b38362da396c6ba5594b214fabf4278c247ae7
CHITRON BHATTACHARJEE <162599803+brandchitron@users.noreply.github.com>
2025-06-28 08:24:51 +0600

    Update category of badwords to "𝗕𝗢𝗫"

--- scripts/cmds/badwords.js
+++ scripts/cmds/badwords.js
@@ -10,7 +10,7 @@ module.exports = {
 			vi: "Bật/tắt/thêm/xóa cảnh báo vi phạm từ thô tục, nếu thành viên vi phạm sẽ bị cảnh báo, lần 2 sẽ kick khỏi box chat",
 			en: "Turn on/off/add/remove bad words warning, if a member violates, he will be warned, the second time he will be kicked out of the chat box"
 		},
-		category: "box chat",
+		category: "𝗕𝗢𝗫",
 		guide: {
 			vi: " {pn} add <words>: thêm từ cấm (có thể thêm nhiều từ cách nhau bằng dấu phẩy \",\" hoặc dấu gạch đứng \"|\""
 				+ "\n {pn} delete <words>: xóa từ cấm (có thể xóa nhiều từ cách nhau bằng dấu phẩy \",\" hoặc dấu gạch đứng \"|\""
