49eae12bcde4cca1efa97b517327bcecc1e3af2f
CHITRON BHATTACHARJEE <162599803+brandchitron@users.noreply.github.com>
2025-06-28 15:47:15 +0600

    Update flickr.js

--- scripts/cmds/flickr.js
+++ scripts/cmds/flickr.js
@@ -17,4 +17,19 @@ module.exports = {
 
   onStart: async function({ message }) {
     const imgUrl = "https://loremflickr.com/600/400";
-    const filePath =
+    const filePath = path.join(__dirname, "cache/flickr.jpg");
+    const file = fs.createWriteStream(filePath);
+
+    https.get(imgUrl, res => {
+      res.pipe(file);
+      file.on("finish", () => {
+        message.reply({
+          body: "📷 𝗥𝗮𝗻𝗱𝗼𝗺 𝗙𝗹𝗶𝗰𝗸𝗿-𝗦𝘁𝘆𝗹𝗲 𝗜𝗺𝗮𝗴𝗲",
+          attachment: fs.createReadStream(filePath)
+        });
+      });
+    }).on("error", () => {
+      message.reply("❌ 𝗙𝗮𝗶𝗹𝗲𝗱 𝘁𝗼 𝗴𝗲𝘁 𝗳𝗹𝗶𝗰𝗸𝗿 𝗶𝗺𝗮𝗴𝗲.");
+    });
+  }
+};
