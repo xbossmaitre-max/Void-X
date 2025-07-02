5529169bf46de54e9dc1df2d3c45e199b5fee6ba
CHITRON BHATTACHARJEE <162599803+brandchitron@users.noreply.github.com>
2025-07-03 01:03:28 +0600

    update author in antichangeinfobox.js

--- scripts/cmds/antichangeinfobox.js
+++ scripts/cmds/antichangeinfobox.js
@@ -4,7 +4,7 @@ module.exports = {
 	config: {
 		name: "antichangeinfobox",
 		version: "1.9",
-		author: "NTKhang",
+		author: "Chitron Bhattacharjee",
 		countDown: 5,
 		role: 0,
 		description: {
