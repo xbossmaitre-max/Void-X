a4475e5437ebc4e45698f9d2458f86b59eceba39
CHITRON BHATTACHARJEE <162599803+brandchitron@users.noreply.github.com>
2025-07-03 01:03:45 +0600

    update author in customrankcard.js

--- scripts/cmds/customrankcard.js
+++ scripts/cmds/customrankcard.js
@@ -8,7 +8,7 @@ module.exports = {
 		name: "customrankcard",
 		aliases: ["crc", "customrank"],
 		version: "1.12",
-		author: "NTKhang",
+		author: "Chitron Bhattacharjee",
 		countDown: 5,
 		role: 0,
 		description: {
